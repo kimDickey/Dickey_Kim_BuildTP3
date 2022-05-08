e314165fc9b89bc00697c6d1c24da7a78897f109
Kim Dickey <1783847@cegepat.qc.ca>
2022-05-08 10:59:46 -0400

    ajout au script du menu

--- Assets/Script/MenuManager.cs
+++ Assets/Script/MenuManager.cs
@@ -7,16 +7,40 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-
+    public Button btnJouer;
+    public Button btnParametre;
+    public Button btnInstructions;
+    public GameObject menuParametre;
+    public GameObject menuInstructions;
     // Start is called before the first frame update
     void Start()
     {
-        
+        btnJouer.onClick.AddListener(btnJouer_Clicked);
+        btnParametre.onClick.AddListener(btnParametre_Clicked);
+        btnInstructions.onClick.AddListener(btnInstructions_Clicked);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void btnJouer_Clicked()
+    {
+        //Affiche message dans la console quand il est cliqué
+        Debug.Log("Bouton jouer à été cliqué");
+    }
+
+    // affiche le menu des parametre
+    void btnParametre_Clicked()
+    {
+        menuParametre.SetActive(true);
+        Debug.Log("Bouton paramètre à été cliqué");
+    }
+    // affiche le menu d'intructions
+    void btnInstructions_Clicked()
+    {
+        menuInstructions.SetActive(true);
+        Debug.Log("Bouton instructions à été cliqué");
     }
 }
