e18ba61cc0b5889d6309f07a7b853103d80bd602
Kim Dickey <1783847@cegepat.qc.ca>
2022-05-13 10:25:02 -0400

    ajout script pour menu pause
    canvas avec les boutons et les points de vie ainsi qu'un timer

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+public class UIManager : MonoBehaviour
+{
+
+    public static UIManager instance;
+    public Text txtTime;
+    public bool isGameOver { get; private set; } = false;
+    
+    float timerDebut;
+
+    public GameObject txtGameOver;
+    public Button btnPause;
+    public Button btnReprendrePartie;
+    public Button btnQuitterPartie;
+    public Button btnQuitterMenu;
+    public GameObject menuPause;
+    public Text txtPointDeVie;
+    public bool isPaused = false;
+
+    int nbVie = 5;
+    public static bool uiIsOpen;
+
+    void Start()
+    {
+        instance = this;
+        // temps du timer départ
+        timerDebut = 0;
+        // quand qu'on clique sur le bouton pause et le jeu se met en pause
+        btnPause.onClick.AddListener(PauseTime);
+        // quand qu'on clique sur le jeu se remet en marche
+        btnReprendrePartie.onClick.AddListener(ResumePartie);
+        btnQuitterMenu.onClick.AddListener(btnQuitterMenu_Clicked);
+        btnPause.onClick.AddListener(btnPause_Clicked);
+        btnQuitterPartie.onClick.AddListener(btnQuitterPartie_Clicked);
+
+    }
+
+    void Update()
+    {
+        // debuter le timer
+        timerDebut = timerDebut + Time.deltaTime;
+        TimeSpan timer = TimeSpan.FromSeconds(timerDebut);
+        // calculer le temps en minutes:secondes
+        txtTime.text = timer.ToString(@"mm\:ss\:ff");
+        Debug.Log(timer.ToString(@"mm\:ss\:ff"));
+        txtPointDeVie.text = nbVie.ToString();
+    }
+    void PauseTime()
+    {
+        // vérifier si le temps est en pause ou non
+        Time.timeScale = 0f;
+        isPaused = true;
+
+    }
+
+    void ResumePartie()
+    {
+        // reprend la partie si le temps n'est plus en pause
+        Time.timeScale = 1f;
+        isPaused = false;
+
+    }
+
+    public void PointDeViePerdu()
+    {
+        // point de vie
+        nbVie -= 1;
+
+        // si le nombre de point est égale 0 la partie est terminer
+        if (nbVie == 0)
+        {
+            GameOver();
+        }
+
+    }
+    // Fin de la partie
+    public void GameOver()
+    {
+        isGameOver = true;
+        txtGameOver.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    void btnPause_Clicked()
+    {
+        // fermer le menuParametre
+        menuPause.SetActive(true);
+        //Affiche message dans la console quand il est cliqué
+        Debug.Log("Bouton pause a été cliqué");
+    }
+    void btnQuitterMenu_Clicked()
+    {
+        // fermer le menuParametre
+        menuPause.SetActive(false);
+        //Affiche message dans la console quand il est cliqué
+        Debug.Log("Bouton quitter à été cliqué");
+    }
+    void btnQuitterPartie_Clicked()
+    {
+        //charge la scene Main
+        SceneManager.LoadScene("MenuAccueil");
+        //Affiche message dans la console quand il est cliqué
+        Debug.Log("Bouton quitter à été cliqué");
+    }
+}
